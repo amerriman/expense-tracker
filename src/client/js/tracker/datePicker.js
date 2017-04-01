d8dc49531ae7c44e73cb3ac4cfab3ae8179a2493
amerriman <amm434@gmail.com>
2017-04-01 12:10:30 -0600

    datepicker bug

--- src/client/js/tracker/datePicker.js
+++ src/client/js/tracker/datePicker.js
@@ -1,22 +1,26 @@
 // angular.module('com.expensetracker.directives.datepicker', [])
-app.directive('datepicker', ["$log", function($log) {
+app.directive('datepicker', ["$log", "$timeout", function($log, $timeout) {
     return {
         restrict: 'A',
         require : 'ngModel',
         link: function(vm, element, attrs, ngModelCtrl) {
           //element must have an id and an ng-model
           //grab the id of the elements and append the picker to that
-          var id = '#' + element[0].id;
+          $timeout(function(){
+            var id = '#' + element[0].id;
 
-          $(id).datepicker({
-            format: "mm/dd/yyyy",
-            todayBtn: "linked",
-            todayHighlight: true,
-            autoclose: true
-          }).on('changeDate', function(e) {
-            ngModelCtrl.$setViewValue(e.date);
-            vm.$apply();
+          $(function(){
+            $(id).datepicker({
+              format: "mm/dd/yyyy",
+              todayBtn: "linked",
+              todayHighlight: true,
+              autoclose: true
+            }).on('changeDate', function(e) {
+              ngModelCtrl.$setViewValue(e.date);
+              vm.$apply();
+            });
           });
+        });
 
         }
     };
