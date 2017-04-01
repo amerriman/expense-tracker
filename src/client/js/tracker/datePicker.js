ab1f0a45700be55aa1aba4d7eab6f96caf955aa3
amerriman <amm434@gmail.com>
2017-04-01 12:46:16 -0600

    datepicker take 3

--- src/client/js/tracker/datePicker.js
+++ src/client/js/tracker/datePicker.js
@@ -1,16 +1,17 @@
 // angular.module('com.expensetracker.directives.datepicker', [])
 app.directive('datepicker', ["$log", "$timeout", function($log, $timeout) {
     return {
-        restrict: 'A',
-        require : 'ngModel',
+        restrict: 'AE',
+        require : '?ngModel',
         link: function(vm, element, attrs, ngModelCtrl) {
           //element must have an id and an ng-model
           //grab the id of the elements and append the picker to that
           $timeout(function(){
-            var id = '#' + element[0].id;
+            // var id = '#' + element[0].id;
+            var id = element[0].id;
 
 
-            $(id).datepicker({
+            $('#date-picker').datepicker({
               format: "mm/dd/yyyy",
               todayBtn: "linked",
               todayHighlight: true,
