f77764ad43f3a7a3522b03891add424c1c5eba9e
amerriman <amm434@gmail.com>
2017-03-31 20:33:59 -0600

    update config

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 var config = {
   TOKEN_SECRET: process.env.TOKEN_SECRET,
-  SALT_WORK_FACTOR: 10,
+  SALT_WORK_FACTOR: process.env.SALT_WORK_FACTOR,
   GOOGLE_SECRET: process.env.GOOGLE_SECRET,
   GOOGLE_CLIENTID: process.env.GOOGLE_CLIENTID
 };
 
 module.exports = config;
+
---
+++ src/client/js/_config2.js
@@ -0,0 +1,1 @@
+var GOOGLE_CLIENTID = '481835593158-bs0v4vmh7m7ml5403le4innvim15arc5.apps.googleusercontent.com';
