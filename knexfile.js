01ad2d4e2e1471c5165e9513ceec26bf2e3c89f9
amerriman <amm434@gmail.com>
2017-03-31 19:23:48 -0600

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL + '?ssl=true',
     migrations: {
       directory: __dirname + '/db/migrations'
     },
