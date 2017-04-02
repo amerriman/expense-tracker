3b9980b51891b05c4cdbe0b38c794e83d9e6b017
amerriman <amm434@gmail.com>
2017-04-01 18:39:10 -0600

    datepicker template update

--- src/client/js/tracker/datePicker.js
+++ src/client/js/tracker/datePicker.js
@@ -2,17 +2,12 @@
 app.directive('datepicker', ["$log", "$timeout", function($log, $timeout) {
     return {
         restrict: 'AE',
-        require : '?ngModel',
+        require : 'ngModel',
         templateUrl: '../../templates/datepicker.html',
         link: function(vm, element, attrs, ngModelCtrl) {
-          //element must have an id and an ng-model
-          //grab the id of the elements and append the picker to that
+          //element must have an ng-model
 
-            var id = '#' + element[0].id;
-            // var id = element[0].id;
-
-
-            $(id).datepicker({
+            $('#date-picker').datepicker({
               format: "mm/dd/yyyy",
               todayBtn: "linked",
               todayHighlight: true,
@@ -21,8 +16,6 @@ app.directive('datepicker', ["$log", "$timeout", function($log, $timeout) {
               ngModelCtrl.$setViewValue(e.date);
               vm.$apply();
             });
-
-
 
         }
     };
