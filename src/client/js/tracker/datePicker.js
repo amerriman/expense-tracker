0c228a9a9c99d70aa1b9ee2ea9a52cfc122325f2
amerriman <amm434@gmail.com>
2017-04-01 18:59:30 -0600

    datepickers galore

--- src/client/js/tracker/datePicker.js
+++ src/client/js/tracker/datePicker.js
@@ -1,23 +1,31 @@
-// app.directive('datepicker', ["$log", "$timeout", function($log, $timeout) {
-//     return {
-//         restrict: 'E',
-//         require : 'ngModel',
-//         templateUrl: '../../templates/datepicker.html',
-//         link: function(vm, element, attrs, ngModelCtrl) {
-//           //element must have an ng-model
-//           console.log(element, "element")
+// angular.module('com.expensetracker.directives.datepicker', [])
+app.directive('datepicker', ["$log", "$timeout", function($log, $timeout) {
+    return {
+        restrict: 'AE',
+        require : 'ngModel',
+        // templateUrl: '../../templates/datepicker.html',
+        link: function(vm, element, attrs, ngModelCtrl) {
+          //element must have an id and an ng-model
+          //grab the id of the elements and append the picker to that
+            // vm.element = element;
+            // var id = '#' + element[0].children[0].id;
 
-//             // $('#date-picker').datepicker({
-//             //   format: "mm/dd/yyyy",
-//             //   todayBtn: "linked",
-//             //   todayHighlight: true,
-//             //   autoclose: true
-//             // }).on('changeDate', function(e) {
-//             //   ngModelCtrl.$setViewValue(e.date);
-//             //   vm.$apply();
-//             // });
+            var id = '#' + element[0].id;
 
-//         }
-//     };
-// }]);
+
+            $(id).datepicker({
+              format: "mm/dd/yyyy",
+              todayBtn: "linked",
+              todayHighlight: true,
+              autoclose: true
+            }).on('changeDate', function(e) {
+              ngModelCtrl.$setViewValue(e.date);
+              vm.$apply();
+            });
+
+
+
+        }
+    };
+}]);
 
