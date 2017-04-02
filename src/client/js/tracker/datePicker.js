2f166fdc9771d55bc26248dec243aebc9b9a872c
amerriman <amm434@gmail.com>
2017-04-01 18:52:25 -0600

    remove all of date picker

--- src/client/js/tracker/datePicker.js
+++ src/client/js/tracker/datePicker.js
@@ -1,23 +1,23 @@
-app.directive('datepicker', ["$log", "$timeout", function($log, $timeout) {
-    return {
-        restrict: 'E',
-        require : 'ngModel',
-        templateUrl: '../../templates/datepicker.html',
-        link: function(vm, element, attrs, ngModelCtrl) {
-          //element must have an ng-model
-          console.log(element, "element")
+// app.directive('datepicker', ["$log", "$timeout", function($log, $timeout) {
+//     return {
+//         restrict: 'E',
+//         require : 'ngModel',
+//         templateUrl: '../../templates/datepicker.html',
+//         link: function(vm, element, attrs, ngModelCtrl) {
+//           //element must have an ng-model
+//           console.log(element, "element")
 
-            // $('#date-picker').datepicker({
-            //   format: "mm/dd/yyyy",
-            //   todayBtn: "linked",
-            //   todayHighlight: true,
-            //   autoclose: true
-            // }).on('changeDate', function(e) {
-            //   ngModelCtrl.$setViewValue(e.date);
-            //   vm.$apply();
-            // });
+//             // $('#date-picker').datepicker({
+//             //   format: "mm/dd/yyyy",
+//             //   todayBtn: "linked",
+//             //   todayHighlight: true,
+//             //   autoclose: true
+//             // }).on('changeDate', function(e) {
+//             //   ngModelCtrl.$setViewValue(e.date);
+//             //   vm.$apply();
+//             // });
 
-        }
-    };
-}]);
+//         }
+//     };
+// }]);
 
