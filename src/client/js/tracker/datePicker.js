ac81276ca35a4fa698a7cc9f9e892708d140d194
amerriman <amm434@gmail.com>
2017-04-01 18:46:51 -0600

    date picker version 45

--- src/client/js/tracker/datePicker.js
+++ src/client/js/tracker/datePicker.js
@@ -1,21 +1,21 @@
-// angular.module('com.expensetracker.directives.datepicker', [])
 app.directive('datepicker', ["$log", "$timeout", function($log, $timeout) {
     return {
-        restrict: 'AE',
+        restrict: 'E',
         require : 'ngModel',
         templateUrl: '../../templates/datepicker.html',
         link: function(vm, element, attrs, ngModelCtrl) {
           //element must have an ng-model
+          console.log(element, "element")
 
-            $('#date-picker').datepicker({
-              format: "mm/dd/yyyy",
-              todayBtn: "linked",
-              todayHighlight: true,
-              autoclose: true
-            }).on('changeDate', function(e) {
-              ngModelCtrl.$setViewValue(e.date);
-              vm.$apply();
-            });
+            // $('#date-picker').datepicker({
+            //   format: "mm/dd/yyyy",
+            //   todayBtn: "linked",
+            //   todayHighlight: true,
+            //   autoclose: true
+            // }).on('changeDate', function(e) {
+            //   ngModelCtrl.$setViewValue(e.date);
+            //   vm.$apply();
+            // });
 
         }
     };
